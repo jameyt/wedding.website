fbc09f9617fc34f7c8ca4ce5eae81d906fc3a20a
Jamey Tyler <jameytyler@outlook.com>
2014-04-07 08:37:51 -0500

    Initial commint

---
+++ weddingwebsite/weddingwebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(weddingwebsite.Startup))]
+namespace weddingwebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
